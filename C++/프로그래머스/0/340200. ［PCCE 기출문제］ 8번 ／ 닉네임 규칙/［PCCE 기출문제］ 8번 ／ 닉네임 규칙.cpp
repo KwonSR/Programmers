0713730df79765aca1c33672c5e46dbd9b4d6a7f
KwonSR <40029560+KwonSR@users.noreply.github.com>
2025-02-10 18:53:38 +0900

    Update ［PCCE 기출문제］ 8번 ／ 닉네임 규칙.cpp

--- C++/프로그래머스/0/340200. ［PCCE 기출문제］ 8번 ／ 닉네임 규칙/［PCCE 기출문제］ 8번 ／ 닉네임 규칙.cpp
+++ C++/프로그래머스/0/340200. ［PCCE 기출문제］ 8번 ／ 닉네임 규칙/［PCCE 기출문제］ 8번 ／ 닉네임 규칙.cpp
@@ -22,9 +22,17 @@ string solution(string nickname) {
             answer += nickname[i];
         }
     }
+    
+    // 단순히 if문만 수정했는데
     while(answer.size() < 4){
         answer += "o";
     }
+    //if 문을 유지한 상태로 같은 결과를 낼 수 있었다.
+    //if(answer.size() < 3){
+    //    answer.append(4-answer.length(),'o');
+    //}
+
+    
     if(answer.size() > 8){
         answer = answer.substr(0,8);
     }
