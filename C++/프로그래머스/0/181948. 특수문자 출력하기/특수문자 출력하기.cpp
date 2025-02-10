d7da5a5103473713e58143b40fb9701c16ee525b
KwonSR <40029560+KwonSR@users.noreply.github.com>
2025-02-11 00:13:24 +0900

    [level 0] Title: 특수문자 출력하기, Time: 1.95 ms, Memory: 4.21 MB -BaekjoonHub

--- C++/프로그래머스/0/181948. 특수문자 출력하기/특수문자 출력하기.cpp
+++ C++/프로그래머스/0/181948. 특수문자 출력하기/특수문자 출력하기.cpp
@@ -3,6 +3,11 @@
 using namespace std;
 
 int main(void) {
-    cout << "!@#$%^&*(\\\'\"<>?:;";
+    
+    // (1) 일반적인 출력방법
+    // cout << "!@#$%^&*(\\\'\"<>?:;";
+    
+    //Raw String 을 이용한 방법  R"문자열"
+    cout << R"(!@#$%^&*(\'"<>?:;)";
     return 0;
 }
