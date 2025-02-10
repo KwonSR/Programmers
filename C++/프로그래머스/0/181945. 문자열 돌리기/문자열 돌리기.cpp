db6a25cf9169ed8eb4392de08723ae0f7c18c4ec
KwonSR <40029560+KwonSR@users.noreply.github.com>
2025-02-11 00:18:21 +0900

    [level 0] Title: 문자열 돌리기, Time: 2.43 ms, Memory: 4.14 MB -BaekjoonHub

--- C++/프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.cpp
+++ C++/프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.cpp
@@ -6,7 +6,14 @@ using namespace std;
 int main(void) {
     string str;
     cin >> str;
-    for(auto c : str)
+    
+    for(const auto c : str)
         cout << c << endl;
+    
+    // for(const char c : str)
+    //     cout << c << endl;
+    
+    // for(auto c : str)
+    //     cout << c << endl;
     return 0;
 }
