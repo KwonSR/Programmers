c3569e60f7c7ab0431f842effc1d832ff9c0231e
KwonSR <40029560+KwonSR@users.noreply.github.com>
2025-02-11 19:30:42 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.02 ms, Memory: 4.19 MB -BaekjoonHub

--- C++/프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.cpp
+++ C++/프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.cpp
@@ -7,14 +7,23 @@ int solution(vector<int> num_list) {
     int answer = 0;
     
     string s1, s2;
-    for(int i = 0 ; i < num_list.size() ; i++)
+    // for(int i = 0 ; i < num_list.size() ; i++)
+    // {
+    //     if(num_list[i] % 2 == 1)
+    //         s1 += to_string(num_list[i]);
+    //     else
+    //         s2 += to_string(num_list[i]);
+    // }
+    // answer = stoi(s1) + stoi(s2);
+    
+    for(auto num : num_list)
     {
-        if(num_list[i] % 2 == 1)
-            s1 += to_string(num_list[i]);
+        if(num%2)
+            s1 += to_string(num);
         else
-            s2 += to_string(num_list[i]);
+            s2 += to_string(num);
     }
-    answer = stoi(s1) + stoi(s2);  
+    answer = stoi(s1) + stoi(s2);
     
     return answer;
 }
