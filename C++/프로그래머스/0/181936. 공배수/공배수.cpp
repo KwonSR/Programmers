dc270e66066f17cf426da18d132cc07d5f03535d
KwonSR <40029560+KwonSR@users.noreply.github.com>
2025-02-11 15:53:00 +0900

    [level 0] Title: 공배수, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- C++/프로그래머스/0/181936. 공배수/공배수.cpp
+++ C++/프로그래머스/0/181936. 공배수/공배수.cpp
@@ -4,10 +4,14 @@
 using namespace std;
 
 int solution(int number, int n, int m) {
+    // (1)
     int answer = 0;
     if(number % n == 0 && number % m == 0)
         answer = 1;
     else
         answer = 0;
     return answer;
+    
+    // (2)
+    // return number%n==0 && number%m == 0 ? 1:0;
 }
