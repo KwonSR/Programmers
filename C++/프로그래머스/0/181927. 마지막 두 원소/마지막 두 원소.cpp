710995ba1f4aa1dbb6e2a0f56dcdbfd82817c0f3
KwonSR <40029560+KwonSR@users.noreply.github.com>
2025-02-11 19:45:32 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ C++/프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> num_list) {
+    vector<int> answer;
+    
+    num_list.back() > num_list.at(num_list.size() -2) ? num_list.push_back(num_list.back() - num_list.at(num_list.size() -2) ) : num_list.push_back(num_list.back() * 2);
+    
+    return num_list;
+}
