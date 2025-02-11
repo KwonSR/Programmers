4408a1deaf8db3b39ad40185525d57cc551c3f62
KwonSR <40029560+KwonSR@users.noreply.github.com>
2025-02-11 20:15:10 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 0.27 ms, Memory: 3.85 MB -BaekjoonHub

---
+++ C++/프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr, vector<vector<int>> queries) {
+    vector<int> answer = arr;
+    
+    int tmp;
+    for(auto a : queries)
+    {
+        tmp = answer[a[0]];
+        answer[a[0]] = answer[a[1]];
+        answer[a[1]] = tmp;
+    }  
+    return answer;
+}
