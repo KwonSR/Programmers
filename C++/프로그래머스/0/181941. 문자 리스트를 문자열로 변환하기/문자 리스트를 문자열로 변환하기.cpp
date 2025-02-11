b233c2964f47ff1b33089c6c0cacac001e2e7d90
KwonSR <40029560+KwonSR@users.noreply.github.com>
2025-02-11 15:12:37 +0900

    [level 0] Title: 문자 리스트를 문자열로 변환하기, Time: 0.03 ms, Memory: 3.6 MB -BaekjoonHub

--- C++/프로그래머스/0/181941. 문자 리스트를 문자열로 변환하기/문자 리스트를 문자열로 변환하기.cpp
+++ C++/프로그래머스/0/181941. 문자 리스트를 문자열로 변환하기/문자 리스트를 문자열로 변환하기.cpp
@@ -10,4 +10,10 @@ string solution(vector<string> arr) {
         answer += c;
     
     return answer;
+    // (1) accumulate() 특정 배열 혹은 container의 누적 합을 구할 때 사용
+    // return accumulate(arr.begin(), arr.end(), string(""));
+    
+    // (2) reduce() => 컨테이너 c의 모든 원소 x에 적용하여 하나의 값으로 축약하는 연산
+    // cbegin(), cend() => const_iterator 타입을 받기위해
+    // return reduce(arr.cbegin(), arr.cend(), string{""});
 }
