5d324fa17c36df6018227e440eeb3e534f181a58
KwonSR <40029560+KwonSR@users.noreply.github.com>
2025-02-11 16:38:03 +0900

    [level 0] Title: 코드 처리하기, Time: 0.53 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ C++/프로그래머스/0/181932. 코드 처리하기/코드 처리하기.cpp
@@ -0,0 +1,37 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string code) {
+    string answer = "";
+    bool mode = 0;
+    
+    for(int i = 0 ; i < code.size() ; i++)
+    {
+        if(mode == 0)
+        {
+            if(code[i] != '1' && i%2==0)
+                answer += code[i];
+            else if(code[i] == '1')
+                mode = !mode;
+        }
+        else
+        {
+            if(code[i] != '1' && i%2==1)
+                answer += code[i];
+            else if(code[i] == '1')
+                mode = !mode;
+        }
+        
+        
+        
+        
+        
+        
+    }
+    
+    
+    
+    return answer.size() > 0 ? answer : "EMPTY";
+}
