40dd4120e0815580dce70b3b3018342a8d2e52ea
KwonSR <40029560+KwonSR@users.noreply.github.com>
2025-02-11 19:08:23 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.01 ms, Memory: 4.12 MB -BaekjoonHub

---
+++ C++/프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+int solution(int a, int b, int c) {
+    // set container
+    // 연관 컨테이너 / 노드 기반 컨테이너, 균형 이진트리
+    // key 원소의 집합 / key값 중복이 안됨
+    // insert 삽입 -> 자동 정렬 (default 오름차순)
+    set<int> num{a, b, c};
+    if(num.size() == 3)
+        return a+b+c;
+    else if(num.size() == 2)
+        return (a+b+c)*(a*a + b*b + c*c);
+    else
+        return (a+b+c) * (a*a + b*b + c*c) * (a*a*a + b*b*b + c*c*c);    
+}
