81d03f9e4c1af849892e068eb85f326afa029606
KwonSR <40029560+KwonSR@users.noreply.github.com>
2025-02-14 00:01:48 +0900

    [level 0] Title: 문자열을 정수로 변환하기, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

--- C++/프로그래머스/0/181848. 문자열을 정수로 변환하기/문자열을 정수로 변환하기.cpp
+++ C++/프로그래머스/0/181848. 문자열을 정수로 변환하기/문자열을 정수로 변환하기.cpp
@@ -5,4 +5,16 @@ using namespace std;
 
 int solution(string n_str) {
     return stoi(n_str);
+
+    // ASCII 코드에서 문자 '0'은 10진수 48, 문자 '9'는 57
+    // n_str[i]에서 '0'의 값을 빼면 10진수 값이 나옴
+    // (ex-  '8' - '0' = 56(10진수) - 48(10진수) => 8)
+    // answer*10 <- 자리수를 하나씩 늘려가기 위해 
+    
+    // for(int i = 0; n_str[i]; i++)
+    // {
+    //     answer = answer*10 +n_str[i]-'0';   
+    // }
 }
+
+    
