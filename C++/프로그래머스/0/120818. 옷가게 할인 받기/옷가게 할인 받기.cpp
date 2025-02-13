7f76518c90d4c05116ac2e7e13d586dda37a9ff7
KwonSR <40029560+KwonSR@users.noreply.github.com>
2025-02-13 17:41:34 +0900

    Update 옷가게 할인 받기.cpp

--- C++/프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.cpp
+++ C++/프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.cpp
@@ -5,10 +5,14 @@ using namespace std;
 
 int solution(int price) {
     int answer = 0;
-    if (price >= 500000) answer = price * 0.8;
-    else if (price >= 300000) answer = price * 0.9;
-    else if (price >= 100000) answer = price * 0.95;
-    else answer = price;
+    if (price >= 500'000)
+        answer = price * 0.8;
+    else if (price >= 300'000)
+        answer = price * 0.9;
+    else if (price >= 100'000)
+        answer = price * 0.95;
+    else
+        answer = price;
+    
     return answer;
-    // return price/10 >= 50000 ? (price*0.8) : price/10 >= 30000 ? (price*0.9) : (price*0.95);
 }
