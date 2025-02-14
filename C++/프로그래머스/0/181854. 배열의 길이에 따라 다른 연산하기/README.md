0f8b20dc2c02944243ec09e9f4f8d7cad4859f69
KwonSR <40029560+KwonSR@users.noreply.github.com>
2025-02-14 17:01:44 +0900

    [level 0] Title: 배열의 길이에 따라 다른 연산하기, Time: 0.32 ms, Memory: 4.02 MB -BaekjoonHub

--- C++/프로그래머스/0/181854. 배열의 길이에 따라 다른 연산하기/README.md
+++ C++/프로그래머스/0/181854. 배열의 길이에 따라 다른 연산하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 배열의 길이에 따라 다른 연산하기 - 181854 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181854) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181854?language=cpp) 
 
 ### 성능 요약
 
-메모리: 4.22 MB, 시간: 0.34 ms
+메모리: 4.02 MB, 시간: 0.32 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 14일 16:57:55
+2025년 02월 14일 17:01:43
 
 ### 문제 설명
 
--- C++/프로그래머스/0/181854. 배열의 길이에 따라 다른 연산하기/배열의 길이에 따라 다른 연산하기.cpp
+++ C++/프로그래머스/0/181854. 배열의 길이에 따라 다른 연산하기/배열의 길이에 따라 다른 연산하기.cpp
@@ -5,17 +5,20 @@ using namespace std;
 
 vector<int> solution(vector<int> arr, int n) {
     vector<int> answer(arr);
+
+    for(int i = (answer.size()+1)%2 ; i < answer.size() ; i+=2)
+        answer[i] += n;
     
-    if(arr.size() % 2 == 1)
-    {
-        for(int i = 0 ; i < answer.size() ; i+=2)
-            answer[i] += n;
-    }
-    else
-    {
-        for(int i = 1 ; i < answer.size() ; i+=2)
-            answer[i] += n;
-    }
+    // if(arr.size() % 2 == 1)
+    // {
+    //     for(int i = 0 ; i < answer.size() ; i+=2)
+    //         answer[i] += n;
+    // }
+    // else
+    // {
+    //     for(int i = 1 ; i < answer.size() ; i+=2)
+    //         answer[i] += n;
+    // }
     
     return answer;
 }
