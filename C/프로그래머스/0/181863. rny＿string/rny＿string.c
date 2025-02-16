5435ec596f243bb8e36816b24ecb10b90008065d
KwonSR <40029560+KwonSR@users.noreply.github.com>
2025-02-17 02:26:54 +0900

    Update rny＿string.c

--- C/프로그래머스/0/181863. rny＿string/rny＿string.c
+++ C/프로그래머스/0/181863. rny＿string/rny＿string.c
@@ -5,6 +5,7 @@
 // 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
 char* solution(const char* rny_string) {
     // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
+    
     int m_count = 0;
     for(int i = 0 ; i < strlen(rny_string) ; i++)
     {
@@ -12,7 +13,7 @@ char* solution(const char* rny_string) {
             m_count += 1;
     }
     
-    char* answer = (char*)malloc(     (strlen(rny_string) + (m_count)*2) * sizeof(char)     );
+    char* answer = (char*)malloc((strlen(rny_string) + (m_count)*2) * sizeof(char));
     
     int idx = 0;
     for(int i = 0 ; i < strlen(rny_string) ; i++)
