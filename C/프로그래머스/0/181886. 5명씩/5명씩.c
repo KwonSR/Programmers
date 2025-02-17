c27ac1b06ee0ae5375082a269f5d575c2b722288
KwonSR <40029560+KwonSR@users.noreply.github.com>
2025-02-18 01:16:35 +0900

    [level 0] Title: 5명씩, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- C/프로그래머스/0/181886. 5명씩/5명씩.c
+++ C/프로그래머스/0/181886. 5명씩/5명씩.c
@@ -8,28 +8,10 @@ char** solution(const char* names[], size_t names_len) {
     // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
     
     int len = names_len%5 != 0 ? names_len/5 + 1 : names_len/5;
-    char** answer = (char**)malloc(sizeof(char*) * len + 1);
+    char** answer = (char**)malloc(sizeof(char*) * len);
     
-    int name_len = 0;
-    for(int i = 0 ; i < names_len ; i++)
-    {
-        if(i%5 == 0)
-        {
-            name_len = strlen(names[i]);
-            answer[i/5] = (char*)malloc(sizeof(char) * name_len + 1);
-        }
-    }
-    
-    for(int i = 0 ; i < names_len ; i++)
-    {
-        if(i%5 == 0)
-        {
-            int j = 0 ;
-            for(j = 0 ; j < strlen(names[i]) ; j++)
-                answer[i/5][j] = names[i][j];
-            answer[i/5][j] = '\0';
-        }
-    }
+    for(int i = 0 ; i < len ; i++)
+        answer[i] = names[i*5];
     
     return answer;
 }
--- C/프로그래머스/0/181886. 5명씩/README.md
+++ C/프로그래머스/0/181886. 5명씩/README.md
@@ -1,10 +1,10 @@
 # [level 0] 5명씩 - 181886 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181886) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181886?language=c) 
 
 ### 성능 요약
 
-메모리: 4.38 MB, 시간: 0.02 ms
+메모리: 4.21 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 18일 01:08:08
+2025년 02월 18일 01:16:34
 
 ### 문제 설명
 
