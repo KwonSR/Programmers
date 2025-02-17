c83c6b7b64163ba7a7b954e318c7ef77cde74283
KwonSR <40029560+KwonSR@users.noreply.github.com>
2025-02-18 01:25:48 +0900

    [level 0] Title: n개 간격의 원소들, Time: 0.01 ms, Memory: 4.23 MB -BaekjoonHub

--- C/프로그래머스/0/181888. n개 간격의 원소들/n개 간격의 원소들.c
+++ C/프로그래머스/0/181888. n개 간격의 원소들/n개 간격의 원소들.c
@@ -6,7 +6,8 @@
 int* solution(int num_list[], size_t num_list_len, int n) {
     // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
     
-    int len = (num_list_len - 1) / n + 1;
+    // int len = (num_list_len - 1) / n + 1;
+    int len = num_list_len / n == 0 ? num_list_len/n : num_list_len/n + 1;
     int* answer = (int*)malloc(sizeof(int) * len);
     
     for(int i = 0 ; i < len ; i++)
