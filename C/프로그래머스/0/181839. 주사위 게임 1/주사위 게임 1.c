88f6b5443e400f5beea964e673a546d582c85ed4
KwonSR <40029560+KwonSR@users.noreply.github.com>
2025-02-18 23:25:14 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.03 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ C/프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int a, int b) {
+    if(a%2 == 1 && b%2 == 1)
+        return a*a + b*b;
+    else if (a%2 == 0 && b%2 == 0)
+        return abs(a-b);
+    else
+        return 2*(a+b);
+}
