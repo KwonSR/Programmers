d1383561ef34e261f5c66981b8c0dc916870dc78
KwonSR <40029560+KwonSR@users.noreply.github.com>
2025-02-18 17:09:22 +0900

    [level 0] Title: 카운트 다운, Time: 0.02 ms, Memory: 4.14 MB -BaekjoonHub

--- C++/프로그래머스/0/181899. 카운트 다운/카운트 다운.cpp
+++ C++/프로그래머스/0/181899. 카운트 다운/카운트 다운.cpp
@@ -6,8 +6,7 @@ using namespace std;
 vector<int> solution(int start_num, int end_num) {
     vector<int> answer;
     
-    int len = start_num - end_num + 1;
-    for(int i = 0 ; i < len ; i++)
+    while(start_num>= end_num)
         answer.push_back(start_num--);
     
     return answer;
