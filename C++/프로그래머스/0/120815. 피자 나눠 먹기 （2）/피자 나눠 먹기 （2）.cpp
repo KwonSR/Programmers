7e30c3f44fff2516dd04a8a924d808b0d8b42e81
KwonSR <40029560+KwonSR@users.noreply.github.com>
2025-02-19 19:27:41 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.01 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ C++/프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    
+    int v = 0;
+    for(int i = 1 ; i <= 6 && i <= n ; i++)
+    {
+        if (6 % i == 0 && n % i == 0)
+            v = i;
+    }
+    
+    return ((n*6)/v)/6;
+}
