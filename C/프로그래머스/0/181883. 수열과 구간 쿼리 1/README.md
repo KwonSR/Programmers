5a45c3ce37823c0b57ac87681d705b2a602f8830
KwonSR <40029560+KwonSR@users.noreply.github.com>
2025-02-21 00:17:22 +0900

    [level 0] Title: 수열과 구간 쿼리 1, Time: 0.25 ms, Memory: 4.21 MB -BaekjoonHub

--- C/프로그래머스/0/181883. 수열과 구간 쿼리 1/README.md
+++ C/프로그래머스/0/181883. 수열과 구간 쿼리 1/README.md
@@ -1,10 +1,10 @@
 # [level 0] 수열과 구간 쿼리 1 - 181883 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181883) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181883?language=c) 
 
 ### 성능 요약
 
-메모리: 4.45 MB, 시간: 0.21 ms
+메모리: 4.21 MB, 시간: 0.25 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 21일 00:15:55
+2025년 02월 21일 00:17:21
 
 ### 문제 설명
 
--- C/프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.c
+++ C/프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.c
@@ -6,7 +6,9 @@
 // queries_rows는 2차원 배열 queries의 행 길이, queries_cols는 2차원 배열 queries의 열 길이입니다.
 int* solution(int arr[], size_t arr_len, int** queries, size_t queries_rows, size_t queries_cols) {
     // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
-    // int* answer = (int*)malloc(sizeof(int) * arr_len);
+    int* answer = (int*)malloc(sizeof(int) * arr_len);
+    for(int i = 0 ; i < arr_len ; i++)
+        answer[i] = arr[i];
     
     for(int i = 0 ; i < queries_rows ; i++)
     {
@@ -14,8 +16,8 @@ int* solution(int arr[], size_t arr_len, int** queries, size_t queries_rows, siz
         int end = queries[i][1];
 
         for(int i = start ; i <= end ; i++)
-            arr[i]++;
+            answer[i]++;
     }
     
-    return arr;
+    return answer;
 }
