f4487dcaf0f6215c8d2cb5dc4beb1ac3fc6d02b6
KwonSR <40029560+KwonSR@users.noreply.github.com>
2025-02-20 23:07:03 +0900

    [level 0] Title: 특별한 이차원 배열 1, Time: 2.82 ms, Memory: 5.23 MB -BaekjoonHub

--- C++/프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.cpp
+++ C++/프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.cpp
@@ -20,4 +20,11 @@ vector<vector<int>> solution(int n) {
     }
     
     return answer;
+    
+    
+//     vector<vector<int>> answer(n, vector<int>(n, 0));
+
+//     for(int i = 0; i < n; ++i)
+//         answer[i][i] = 1;
+//     return answer;
 }
