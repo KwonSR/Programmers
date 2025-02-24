0a50f535a63b9c144ec25c1d3eae9ee144875113
KwonSR <40029560+KwonSR@users.noreply.github.com>
2025-02-24 21:13:42 +0900

    [level 0] Title: 배열 만들기 5, Time: 4.20 ms, Memory: 6.32 MB -BaekjoonHub

---
+++ C++/프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<string> intStrs, int k, int s, int l) {
+    vector<int> answer;
+    
+    for(auto str : intStrs)
+    {
+        int tmp = stoi(str.substr(s, l));
+        if( tmp > k )
+            answer.push_back(tmp);
+    }
+    
+    return answer;
+}
