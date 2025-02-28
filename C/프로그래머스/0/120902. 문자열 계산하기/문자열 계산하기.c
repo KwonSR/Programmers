fd6ab1e5dbd6476d9ded5be2928231ba706bc092
KwonSR <40029560+KwonSR@users.noreply.github.com>
2025-03-01 02:17:08 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.01 ms, Memory: 3.63 MB -BaekjoonHub

--- C/프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.c
+++ C/프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.c
@@ -5,34 +5,28 @@
 // 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
 int solution(const char* my_string) {
     int answer = 0;
+    int num = 0;
     int idx = 0;
-    int num1 = 0;
-    int num2 = 0;
-    char oper;
-    while(true)
+    char oper = '+';
+
+    while(idx <= strlen(my_string))
     {
-        num1 = num1 * 10 + my_string[idx] - '0';
-        idx++;
-        if(my_string[idx] == ' ')
-            break;
-    }
-    while(my_string[idx] != '\0')
-    {
-        idx++;
-        oper = my_string[idx];
-        idx += 2;
-        while(true){
-            num2 = num2 * 10 + my_string[idx] - '0'; 
-            idx++;
-            if(my_string[idx] == ' ' || my_string[idx] == '\0')
-                break;
+        if(my_string[idx] <= '9' && my_string[idx] >= '0')
+        {
+            num = num * 10 + my_string[idx] - '0';
+            if(my_string[idx + 1] == ' ' || my_string[idx + 1 ] == '\0')
+            {
+                if(oper == '+')
+                    answer += num;
+                else if(oper == '-')
+                    answer -= num;    
+                num = 0;
+            }
         }
-        if(oper == '+')
-            answer = num1 + num2;
-        if(oper == '-')
-            answer = num1 - num2;
-        num2 = 0;
-        num1 = answer;
+        else if(my_string[idx] == '+' || my_string[idx] == '-')
+            oper = my_string[idx];
+        idx++;
     }
+    
     return answer;
 }
