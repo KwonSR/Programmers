5a29db1bc9e1b95f6167c53d306a58c7bdfada6d
KwonSR <40029560+KwonSR@users.noreply.github.com>
2025-03-02 01:59:26 +0900

    [level 0] Title: 공 던지기, Time: 0.01 ms, Memory: 4.53 MB -BaekjoonHub

--- C/프로그래머스/0/120843. 공 던지기/공 던지기.c
+++ C/프로그래머스/0/120843. 공 던지기/공 던지기.c
@@ -4,11 +4,10 @@
 
 // numbers_len은 배열 numbers의 길이입니다.
 int solution(int numbers[], size_t numbers_len, int k) {
-    int answer = 0, idx = 0;
-    for (int i = 0; i < k - 1; i++) {
-        idx += 2;
-        if (idx > numbers_len) idx = idx % numbers_len;
-    }
+    int answer = 0;
+    int idx = (2 * (k - 1)) % numbers_len;
+    
     answer = numbers[idx];
+    
     return answer;
 }
