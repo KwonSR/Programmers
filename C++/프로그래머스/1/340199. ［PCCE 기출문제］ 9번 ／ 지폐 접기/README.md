a86a7ff55df7c0b1572b38ace83cd42246a600bd
KwonSR <40029560+KwonSR@users.noreply.github.com>
2025-03-12 17:29:34 +0900

    [level 1] Title: [PCCE 기출문제] 9번 / 지폐 접기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- C++/프로그래머스/1/340199. ［PCCE 기출문제］ 9번 ／ 지폐 접기/README.md
+++ C++/프로그래머스/1/340199. ［PCCE 기출문제］ 9번 ／ 지폐 접기/README.md
@@ -1,10 +1,10 @@
 # [level 1] [PCCE 기출문제] 9번 / 지폐 접기 - 340199 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/340199) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/340199?language=cpp) 
 
 ### 성능 요약
 
-메모리: 4.15 MB, 시간: 0.01 ms
+메모리: 4.21 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 12일 17:26:52
+2025년 03월 12일 17:29:31
 
 ### 문제 설명
 
--- C++/프로그래머스/1/340199. ［PCCE 기출문제］ 9번 ／ 지폐 접기/［PCCE 기출문제］ 9번 ／ 지폐 접기.cpp
+++ C++/프로그래머스/1/340199. ［PCCE 기출문제］ 9번 ／ 지폐 접기/［PCCE 기출문제］ 9번 ／ 지폐 접기.cpp
@@ -7,15 +7,20 @@ using namespace std;
 int solution(vector<int> wallet, vector<int> bill) {
     int answer = 0;
     
+    // 오름차순 - 큰 값을 반으로 나누기 때문
     sort(wallet.begin(), wallet.end(), less<int>());
     sort(bill.begin(), bill.end(), less<int>());
     
     int tmp = 0;
+    // 둘 중 하나라도 크면 루프는 계속 돌아감
     while(bill[0] > wallet[0] || bill[1] > wallet[1])
     {
+        // 큰 값을 반으로 나눔
         bill[1] /= 2;
         answer++;
         
+        // 나누고 나서 오름차순인지 값 확인을 해야함
+        // sort 써도되는데 그냥 이렇게 함
         if(bill[0] > bill[1])
         {
             tmp = bill[0];
